d7e43eda84f17bb2bf727be05aedbe41f371031a
JinMay <britzegs@gmail.com>
2017-10-28 11:54:16 +0900

    Make model / admin

--- rest_haksik/menu/admin.py
+++ rest_haksik/menu/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Main, Yangjin, Yangsung, Crj
+
+@admin.register(Main)
+class MainAdmin(admin.ModelAdmin):
+    list_display = ['number', 'day']
+    list_display_links = ['day']
+
+
+@admin.register(Yangsung)
+class YangsungAdmin(admin.ModelAdmin):
+    list_display = ['number', 'day']
+    list_display_links = ['day']
+
+
+@admin.register(Yangjin)
+class YangjinAdmin(admin.ModelAdmin):
+    list_display = ['number', 'day']
+    list_display_links = ['day']
+
+
+@admin.register(Crj)
+class CrjAdmin(admin.ModelAdmin):
+    list_display = ['number', 'day']
+    list_display_links = ['day']
--- rest_haksik/menu/models.py
+++ rest_haksik/menu/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Main(models.Model):
+    number = models.IntegerField()
+    day = models.TextField()
+
+    def __str__(self):
+        return self.day
+
+
+class Yangsung(models.Model):
+    number = models.IntegerField()
+    day = models.TextField()
+
+    def __str__(self):
+        return self.day
+
+
+class Yangjin(models.Model):
+    number = models.IntegerField()
+    day = models.TextField()
+
+    def __str__(self):
+        return self.day
+
+
+class Crj(models.Model):
+    number = models.IntegerField()
+    day = models.TextField()
+
+    def __str__(self):
+        return self.day
