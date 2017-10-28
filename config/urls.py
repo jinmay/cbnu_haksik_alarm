dd83a53cbc395b03ae9abae8f4a7441f21791b2f
JinMay <britzegs@gmail.com>
2017-10-28 11:44:40 +0900

    Init app/menu

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
+    url(r'^menu/', include('rest_haksik.menu.urls', namespace="menu")),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
