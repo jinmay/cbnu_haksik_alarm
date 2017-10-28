fd2121028bbe1fc4c49c8cd03db56d8ffc16bc7a
JinMay <britzegs@gmail.com>
2017-10-28 17:06:10 +0900

    Edit urls.py
    Add crawling url / message url

--- rest_haksik/menu/urls.py
+++ rest_haksik/menu/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 
+from . import views
 from . import crawling
 
 urlpatterns = [
@@ -8,4 +9,9 @@ urlpatterns = [
     url(r'^jin.crawling$', crawling.jin_crawling, name="jin_crawling"),
     url(r'^sung.crawling$', crawling.sung_crawling, name="sung_crawling"),
     url(r'^crj.crawling$', crawling.crj_crawling, name="crj_crawling"),
+
+    # For Kakaotalk auto reply
+    url(r'^keyboard$', views.keyboard, name="keyboard"),
+    url(r'^message$', views.Answer.as_view(), name="answer"),
+
 ]
