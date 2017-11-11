5809e9fd140c30e31eaf1d0025e5a1f8994eb726
JinMay <britzegs@gmail.com>
2017-11-11 20:59:19 +0900

    Add new model for Galaxy restaurant

--- rest_haksik/menu/models.py
+++ rest_haksik/menu/models.py
@@ -38,6 +38,14 @@ class Star(models.Model):
     def __str__(self):
         return self.menu
 
+
+class Galaxy(models.Model):
+    number = models.IntegerField()
+    menu = models.TextField()
+
+    def __str__(self):
+        return self.menu
+
 class User(models.Model):
     key = models.CharField(max_length=100)
     dorm = models.CharField(max_length=50, default="", null=True, blank=True)
