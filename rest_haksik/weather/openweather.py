60fb75dca365925795aea72d009c24acfd59ca4b
JinMay <britzegs@gmail.com>
2018-04-03 09:56:01 +0900

    Edit function to get weather information
    Add weather info - humidity / temperature / cloudiness

--- rest_haksik/weather/openweather.py
+++ rest_haksik/weather/openweather.py
@@ -3,29 +3,32 @@ import pyowm
 import psycopg2
 
 
-# settings.local 에서 불러오고 싶은데 
-# 나름대로 해봐도 못하겠어서 급한대로 아래와 같이 사용
+# settings.local ( fail )
 owm_apikey = os.environ["OWM_APIKEY"]
 
 
 try:
-    conn = psycopg2.connect("dbname=rest_haksik") # psql db 연결하고
-    cur = conn.cursor() # cursor 생성하고
+    conn = psycopg2.connect("dbname=rest_haksik") # connect to psql db
+    cur = conn.cursor() # initialize cursor
 
     owm = pyowm.OWM(owm_apikey)
     observation = owm.weather_at_place('Cheongju,KR')
     weather = observation.get_weather()
 
-    temperature_dict = weather.get_temperature('celsius') # 기본값은 켈빈온도라서 섭씨로 바꿔주어야 함
+    temperature_dict = weather.get_temperature('celsius') # convert Kelvin to celsius
     temperature = temperature_dict['temp']
     temperature = int(temperature)
+
+    clouds = weather.get_clouds()
+    humidity = weather.get_humidity()
+
+    print("구름: {}\n습도: {}%".format(clouds, humidity))
     # print(temperature)
 
-    # cur.execute("INSERT INTO weather_weather (temp) VALUES (%s, )", (temperature)) # 온도 집어넣기!!
-
-    SQL = "INSERT INTO weather_weather (temp) VALUES (%s);"
-    data = (temperature, )
-    cur.execute(SQL, data) # 온도 집어넣기!!
+    # cur.execute("INSERT INTO weather_weather (temp) VALUES (%s, )", (temperature))
+    SQL = "INSERT INTO weather_weather (temp, humidity, clouds) VALUES (%s, %s, %s);"
+    data = (temperature, humidity, clouds)
+    cur.execute(SQL, data)
     conn.commit()
 
 finally:
