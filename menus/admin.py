3602a74b0d1a0adf765fcc816411734a7724baf4
jinmay <britzegs@gmail.com>
2019-03-25 17:13:21 +0900

    모델어드민 추가

--- menus/admin.py
+++ menus/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import (
+                Middle, Yangjin, Yangsung,
+                User,
+            )
 
-# Register your models here.
+@admin.register(Middle)
+class MiddleAdmin(admin.ModelAdmin):
+    list_display = ['number', 'menu']
+    list_display_links = ['menu']
+
+
+@admin.register(Yangsung)
+class YangsungAdmin(admin.ModelAdmin):
+    list_display = ['number', 'menu']
+    list_display_links = ['menu']
+
+
+@admin.register(Yangjin)
+class YangjinAdmin(admin.ModelAdmin):
+    list_display = ['number', 'menu']
+    list_display_links = ['menu']
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['key', 'dorm']
