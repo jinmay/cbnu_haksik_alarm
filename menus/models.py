fed811320a2133a9024cc222b0fdd8fc016cd828
jinmay <britzegs@gmail.com>
2019-03-25 16:57:24 +0900

    모델 생성
    기숙사 식단을 저장하기 위한 모델 생성

--- menus/models.py
+++ menus/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+week_list = ['일요일', '월요일', '화요일', '수요일' ,'목요일', '금요일', '토요일']
+week = {
+    idx: v
+    for idx, v in enumerate(week_list)
+}
+
+class Middle(models.Model):
+    number = models.IntegerField()
+    menu = models.TextField()
+
+    def __str__(self):
+        return self.week[number]
+
+
+class Yangsung(models.Model):
+    number = models.IntegerField()
+    menu = models.TextField()
+
+    def __str__(self):
+        return self.week[number]
+
+
+class Yangjin(models.Model):
+    number = models.IntegerField()
+    menu = models.TextField()
+
+    def __str__(self):
+        return self.week[number]
+
+
+class User(models.Model):
+    key = models.CharField(max_length=100)
+    dorm = models.CharField(max_length=50, default="")
