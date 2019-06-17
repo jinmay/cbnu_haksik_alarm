e6db4238617d6fa205c80abf444c62fa03e9b641
jinmay <britzegs@gmail.com>
2019-06-17 16:16:24 +0900

    rn_api를 위한 크롤링코드 구현 완료

--- rn_api/urls.py
+++ rn_api/urls.py
@@ -1,8 +1,27 @@
 from django.urls import path
 from . import views
+from . import crawling
 
 app_name = "rn"
 
 urlpatterns = [
+    # For RN api
     path('get_today/', views.get_today),
+
+    # For crawling
+    path(
+        route='crawling.main',
+        view=crawling.main_crawling,
+        name='main_crawling'
+    ),
+    path(
+        route='crawling.yangjin',
+        view=crawling.jin_crawling,
+        name='jin_crawling'
+    ),
+    path(
+        route='crawling.yangsung',
+        view=crawling.sung_crawling,
+        name='sung_crawling'
+    ),
 ]
